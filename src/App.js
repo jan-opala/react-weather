a5691c9ad5fd909961bf3413bbd07134dd3f9687
Jan Opala <jan.opala@yahoo.com>
2025-05-21 23:15:11 +0200

    Fix rain coming up where it shouldnt.

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ export const getWeather = async (loc) => {
       icon="thunderstorm.svg";
     } else if ([20, 22, 23, 26, 36, 37, 38, 39, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 83, 84, 85, 86, 87, 88, 89, 90, 93, 94].includes(weather_code)) {
       icon="snow.svg";
-    } else if ([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 80, 81, 82, 83, 84, 85, 86, 91, 92].includes(weather_code)) {
+    } else if ([20, 21, 22, 23, 24, 25, 26, 27, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 80, 81, 82, 83, 84, 85, 86, 91, 92].includes(weather_code)) {
       icon="rain.svg";
     } else if ([50, 51, 52, 53, 54, 55, 56, 57, 58, 59].includes(weather_code)) {
       icon="drizzle.svg";
