57c63bd828d5740e6543f50970ec56f281b2b3a9
Jan Opala <jan.opala@yahoo.com>
2025-05-21 17:53:05 +0200

    Initial commit.  - Basic weather search  - Fetch geocode by query from OpenStreetMap API  - Fetch CURRENT weather data from Open-Meteo API  - Show CURRENT weather data

---
+++ src/App.js
@@ -0,0 +1,157 @@
+import { useState } from "react";
+import axios from 'axios';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image'
+
+import './App.css';
+
+export function Weather( {location, state, lat, lon, temperature, weather_code, icon, humidity, wind} ) {
+  if (temperature === -300) {
+    return (
+    <Container className="text-center mt-5">
+      <h1>Wyszukaj lokalizację.</h1>
+    </Container>
+    );
+  }
+  if (temperature === -404) {
+    return (
+    <Container className="text-center mt-5">
+      <h1>Błąd - nie znaleziono lokalizacji.</h1>
+    </Container>
+    );
+  }
+  return (
+    <Container className="text-center mt-5">
+      <Image className="icon" src={`/weather_icons/` + icon} />
+      <h1>{location}</h1>
+      <p className="state fs-8 text-secondary">{state}</p>
+      <p className="fs-5 mt-2">Temperatura: {temperature}°C</p>
+      <p className="next fs-5">Wilgotność: {humidity} %</p>
+      <p className="next fs-5">Wiatr: {wind} km/h</p>
+    </Container>
+  );
+}
+
+export const fetcherWithAxios = async (url) => {
+  const response = await axios.get(url);
+  return response.data;
+}
+export const getWeather = async (loc) => {
+
+  try {
+    const locData = await fetcherWithAxios(`https://nominatim.openstreetmap.org/search?q=${loc}&format=json&limit=1&featureType=city&addressdetails=1`);
+    const location = `${locData[0].name}, ${locData[0].address.country}`;
+    const state = locData[0].address.state || locData[0].address.province || locData[0].address.region;
+    const lat = locData[0].lat;
+    const lon = locData[0].lon;
+    const weatherData = await fetcherWithAxios(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code,wind_speed_10m,cloud_cover,is_day,relative_humidity_2m`);
+    const temperature = weatherData.current.temperature_2m;
+    const weather_code = weatherData.current.weather_code;
+    const wind_speed = weatherData.current.wind_speed_10m;
+    const cloud_cover = weatherData.current.cloud_cover;
+    const is_day = weatherData.current.is_day;
+    const humidity = weatherData.current.relative_humidity_2m;
+    var icon;
+    console.log(weather_code)
+    if ([17, 29, 91, 92, 93, 94, 95, 96, 99].includes(weather_code)) {
+      icon="thunderstorm.svg";
+    } else if ([20, 22, 23, 26, 36, 37, 38, 39, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 83, 84, 85, 86, 87, 88, 89, 90, 93, 94].includes(weather_code)) {
+      icon="snow.svg";
+    } else if ([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 80, 81, 82, 83, 84, 85, 86, 91, 92].includes(weather_code)) {
+      icon="rain.svg";
+    } else if ([50, 51, 52, 53, 54, 55, 56, 57, 58, 59].includes(weather_code)) {
+      icon="drizzle.svg";
+    } else if ([5, 10, 28, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 76, 77, 78].includes(weather_code)) {
+      icon="fog.svg";
+    } else if (wind_speed >= 38) {
+      icon="windy.svg";
+    } else if (cloud_cover >= 80) {
+      icon="cloudy.svg";
+    } else if (cloud_cover >= 35) {
+      icon="partly_cloudy.svg";
+    } else if (cloud_cover >= 12) {
+      icon="mostly_sunny.svg";
+    } else if (temperature >= 33) {
+      icon="sunny_hot.svg";
+    } else if (is_day === 0) {
+      icon="night.svg";
+    } else {
+      icon="sunny.svg";
+    }
+    return({
+      location: location,
+      state: state,
+      lat: lat,
+      lon: lon,
+      temperature: temperature,
+      icon: icon,
+      humidity: humidity,
+      wind: wind_speed,
+    });
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    return {
+      location: "",
+      state: "",
+      lat: "",
+      lon: "",
+      temperature: -404,
+      icon: "",
+      humidity: 0,
+      wind: 0,
+    };
+  } finally {}  
+
+};
+
+const App = () => {
+  const [location, setLocation] = useState("");
+  const [state, setState] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [temperature, setTemperature] = useState(-300);
+  const [weather_code, setWeatherCode] = useState("");
+  const [icon, setIcon] = useState("");
+  const [humidity, setHumidity] = useState(0);
+  const [wind, setWind] = useState(0);
+  
+  const onFormSubmit = async(e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target),
+    formDataObj = Object.fromEntries(formData.entries())
+    const weatherData = await getWeather(formDataObj.locationInput);
+    setLocation(weatherData.location);
+    setState(weatherData.state);
+    setLatitude(weatherData.lat);
+    setLongitude(weatherData.lon);
+    setTemperature(weatherData.temperature);
+    setWeatherCode(weatherData.weather_code);
+    setIcon(weatherData.icon);
+    setHumidity(weatherData.humidity);
+    setWind(weatherData.wind);
+  }
+
+  return (
+    <Container>
+
+      <Container className="search mt-5 justify-content-center">
+        <Form className="ms-4 me-4 d-flex" onSubmit={onFormSubmit}>
+
+          <Form.Control className="me-3" name="locationInput" type="text" placeholder="Radom, Polska" />
+          <Button variant="primary" type="submit">
+            Szukaj
+          </Button>
+
+        </Form>
+      </Container>
+      
+      <Weather location={location} state={state} lat={latitude} lon={longitude} temperature={temperature} weather_code={weather_code} icon={icon} humidity={humidity} wind={wind} />
+
+    </Container>
+  );
+}
+
+export default App;
